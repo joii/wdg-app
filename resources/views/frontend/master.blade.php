6a06ff92487f9be13163bee17ff5b3001a6791f6
phatchara yubon <contact.puipui@gmail.com>
2025-09-09 16:49:05 +0700

    Update master.blade.php

--- resources/views/frontend/master.blade.php
+++ resources/views/frontend/master.blade.php
@@ -5,11 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale = 1.0,
 maximum-scale=1.0, user-scalable=no" />
 <meta name="apple-mobile-web-app-capable" content="yes"/>
-<link rel="icon" type="image/x-icon" href="{{ asset('frontend/assets/img/favicon.ico')}}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('frontend/assets/img/apple-touch-icon.png')}}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('frontend/assets/img/favicon-32x32.png')}}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('frontend/assets/img/favicon-16x16.png')}}">
-<link rel="manifest" href="/site.webmanifest">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet">
